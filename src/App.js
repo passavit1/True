cf064bb5229e2e6b5a8791bce350038a014e2d5d
Wizat <passavit1@gmail.com>
2023-07-31 22:44:46 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-pascal-case */
 
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {
   Index_page,
   Event1,
@@ -12,29 +12,22 @@ import {
 } from "./Pages/index";
 
 function App() {
-  // const [isLoading, setIsLoading] = useState(false);
-  // const navigate = useNavigate();
 
-  // const handleNavigate = (path) => {
-  //   setIsLoading(true);
-  //   setTimeout(() => {
-  //     setIsLoading(false);
-  //     navigate(path);
-  //   }, 300);
-  // };
 
   return (
     <div>
-      {/* {isLoading && <LoadingPage />} */}
-      <Routes>
-        <Route path="/" element={<Index_page />} />
-        <Route path="event1" element={<Event1 />} />
-        <Route path="event2" element={<Event2 />} />
-        <Route path="event3" element={<Event3 />} />
-        <Route path="event4" element={<Event4 />} />
-        <Route path="event5" element={<Event5 />} />
-        <Route path="Test" element={<Test />} />
-      </Routes>
+       <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index_page />} />
+          <Route path="/event1" element={<Event1 />} />
+          <Route path="/event2" element={<Event2 />} />
+          <Route path="/event3" element={<Event3 />} />
+          <Route path="/event4" element={<Event4 />} />
+          <Route path="/event5" element={<Event5 />} />
+          <Route path="/Test" element={<Test />} />
+        </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
